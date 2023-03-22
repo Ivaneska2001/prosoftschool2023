6a7d8a9f51b0a9ffd0a9210c560ca02d52439b12
Ivaneska2001 <99940167+Ivaneska2001@users.noreply.github.com>
2023-03-23 00:17:34 +0500

    Update first_task.h
    Борис, Привет! Подскажи, пожалуйста, код является неполным. Он сравнивает матрицы только по первой цифры (кроме оператора ==) Есть способ как то этого избежать, имея на руках только this?

--- template_comparision_task/first_task.h
+++ template_comparision_task/first_task.h
@@ -1,4 +1,36 @@
-class NewA
+#include <iostream>
+
+template <class Base1>
+class Base {
+public:
+    template<class T>
+    
+    bool operator==(const T& other) {
+        Base1* there = static_cast<Base1*>(this);
+        return !((*there != other));
+    }
+
+    template<class T>
+    bool operator>=(const T& other) {
+        Base1* there = static_cast<Base1*>(this);
+        return !(*there < other);
+    }
+
+    template<class T>
+    bool operator<=(const T& other) {
+        Base1* there = static_cast<Base1*>(this);
+        return !(*there > other);
+    }
+
+    template<class T>
+    bool operator!=(const T& other) {
+        Base1* there = static_cast<Base1*>(this);
+        return ((*there > other) || (*there < other));
+    }
+   
+};
+
+class NewA : public Base<NewA>
 {
 public:
     NewA(int a, int b) : m_a(a), m_b(b){}
@@ -29,3 +61,21 @@ private:
     int m_a = 0;
     int m_b = 0;
 };
+
+
+  int main()
+{
+    NewA a1(1, 2);
+    NewA a2(1, 2);
+    std::cout << (a1 == a2) << "\n";
+    std::cout << "Boris, das ist meine erste Probe" << "\n";
+
+    NewA a3(3, 1);
+    NewA a4(5, 2);
+    std::cout << (a3 <= a4) << "\n";
+    std::cout << "Boris, das ist meine zweite Probe" << "\n";
+
+    return 0;
+}
+
+
